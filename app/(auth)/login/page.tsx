0472b50eef4cc14e010b21221c93a3058c5d7d31
JuntaLaw <junta_law@me.com>
2023-12-08 08:48:34 +0900

    update

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import Link from "next/link";
-// import GoogleIcon from "../../../public/google.svg"
-// import Image from "next/image";
-import { signIn } from "next-auth/react";
+import { Input } from "@/components/ui/input"; 
+import Link from "next/link"; 
 import GithubSigninButton from "@/app/components/GithubSigninButton";
 import GoogleSigninButton from "@/app/components/GoogleSigninButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+    const session = await getServerSession(authOptions);
+
+    if(session) {
+        return redirect('/home')
+    }
     return (
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form>
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import Link from "next/link";
-import GoogleIcon from "../../../public/google.svg"
-import Image from "next/image";
+import { Input } from "@/components/ui/input"; 
+import Link from "next/link"; 
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/auth";
+import { redirect } from "next/navigation";
+import GithubSigninButton from "@/app/components/GithubSigninButton";
+import GoogleSigninButton from "@/app/components/GoogleSigninButton";
 
-export default function SignUp() {
+export default async function SignUp() {
+    const session = await getServerSession(authOptions);
+
+    if(session) {
+        return redirect('/home')
+    }
     return (
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form>
@@ -25,12 +32,8 @@ export default function SignUp() {
             </div>
 
             <div className="flex w-full justify-center items-center gap-x-6 mt-6">
-                <Button variant="outline" size="icon">
-                    <GithubIcon className="w-6 h-6"/> 
-                </Button>
-                <Button variant="outline" size="icon">
-                    <Image src={GoogleIcon} alt="Google Icon"/>
-                </Button>
+            <GithubSigninButton />
+            <GoogleSigninButton />
             </div>
         </div>
     )
