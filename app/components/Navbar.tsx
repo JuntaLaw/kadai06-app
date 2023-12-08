7f545ce2dda6fcaa2c2b61b1f883f8025e89c649
JuntaLaw <junta_law@me.com>
2023-12-09 02:20:43 +0900

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link"; 
 import Image from "next/image";
 import Logo from '../../public/NYATFLIX.png'
+import { usePathname } from "next/navigation";
+import { Bell, Search } from "lucide-react";
+import UserNav from "./UserNav";
 
 interface linkPrpos {
     name: string;
@@ -15,23 +20,40 @@ const links: linkPrpos[] = [
     {name: 'My List', href: '/home/user/list'},
 ];
 export default function Navbar () {
+    const  pathName = usePathname();
     return (
-        <div 
-        className="w-full ma-w-xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
+        <div className="w-full ma-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
             <div className="flex items-center">
                 <Link href="/home" className="w-32">
                     <Image src={Logo} alt="Nyatflix logo" priority/>
                 </Link>
                 <ul className="lg:flex gap-x-4 ml-14 hidden">
                     {links.map((link, idx) => (
-                        <div key={idx}>
-
+                        <div key={idx}> 
+                        {pathName === link.href ? (
+                            <li>
+                                <Link href={link.href} className="text-white font-semibold underline text-sm">
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ): (
+                            <li>
+                                <Link className="text-gray-300 font-normal text-sm"
+                                href={link.href}>
+                                    {link.name}
+                                </Link>
+                            </li>
+                        )}
                         </div>
                     ))}
 
                 </ul>
             </div>
-
+            <div className="flex items-center gap-x-8">
+                <Search className="w-5 h-5 text-gray-300 cursor-pointer"/>
+                <Bell className="w-5 h-5 text-gray-300 cursor-pointer"/>
+                <UserNav />
+            </div>
         </div>    
     )
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-import Logo from '../../public/NYATFLIX.png' 
+ import Navbar from "../components/Navbar";
 
 export default function HomePage() {
     return (
-        <div 
-        className="w-full ma-w-xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
-            <div className="flex items-center"> 
-            Hello from Home
-            </div>
-
-        </div>    
+        <Navbar /> 
     )
 }
