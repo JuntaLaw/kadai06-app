1970afa053a1cde69443d38b0b863b861e3e2ad0
JuntaLaw <junta_law@me.com>
2023-12-09 18:57:09 +0900

    update

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"; 
-import Link from "next/link"; 
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 import GithubSigninButton from "@/app/components/GithubSigninButton";
 import GoogleSigninButton from "@/app/components/GoogleSigninButton";
 import { getServerSession } from "next-auth";
@@ -10,19 +10,19 @@ import { redirect } from "next/navigation";
 export default async function Login() {
     const session = await getServerSession(authOptions);
 
-    if(session) {
+    if (session) {
         return redirect('/home')
     }
     return (
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-            <form>
+            <form method="post" action="/api/auth/signin">
                 <h1 className="text-3xl font-semibold text-white">Log in</h1>
                 <div className="space-y-4 mt-5">
-                    <Input type="email" name="email" placeholder="Email" 
-                    className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"/>
+                    <Input type="email" name="email" placeholder="Email"
+                        className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block" />
                     <Button type="submit"
-                    variant="destructive"
-                    className="w-full bg-pink-600">Log in</Button>
+                        variant="destructive"
+                        className="w-full bg-pink-600">Log in</Button>
                 </div>
             </form>
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"; 
-import Link from "next/link"; 
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
@@ -10,19 +10,19 @@ import GoogleSigninButton from "@/app/components/GoogleSigninButton";
 export default async function SignUp() {
     const session = await getServerSession(authOptions);
 
-    if(session) {
+    if (session) {
         return redirect('/home')
     }
     return (
         <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-            <form>
+            <form method="post" action="/api/auth/signin">
                 <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
                 <div className="space-y-4 mt-5">
-                    <Input type="email" name="email" placeholder="Email" 
-                    className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"/>
+                    <Input type="email" name="email" placeholder="Email"
+                        className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block" />
                     <Button type="submit"
-                    variant="destructive"
-                    className="w-full bg-pink-600">Sign Up</Button>
+                        variant="destructive"
+                        className="w-full bg-pink-600">Sign Up</Button>
                 </div>
             </form>
 
@@ -32,8 +32,8 @@ export default async function SignUp() {
             </div>
 
             <div className="flex w-full justify-center items-center gap-x-6 mt-6">
-            <GithubSigninButton />
-            <GoogleSigninButton />
+                <GithubSigninButton />
+                <GoogleSigninButton />
             </div>
         </div>
     )
